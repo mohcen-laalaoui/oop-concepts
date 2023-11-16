cdfc877628860cc2d4a2338b1a9ccc7ab8dbbc3e
mohcen-laalaoui <134273543+mohcen-laalaoui@users.noreply.github.com>
2023-11-16 21:56:33 +0100

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-package Inh1;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Car myCar = new Car();
-        myCar.honk();
-        System.out.println(myCar.brand + " " +  myCar.modelName);
-    }
-}
