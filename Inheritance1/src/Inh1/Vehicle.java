2ca5cbf75dd59adecaf441deacdfcd06ce9c5472
mohcen-laalaoui <134273543+mohcen-laalaoui@users.noreply.github.com>
2023-11-17 11:41:22 +0100

    Update Vehicle.java

--- Inheritance1/src/Inh1/Vehicle.java
+++ Inheritance1/src/Inh1/Vehicle.java
@@ -1,7 +1,7 @@
 package Inh1;
+  /*create a super class */
 public class Vehicle {
     protected String brand = "Ford";
-
     public void honk() {
         System.out.println("Tuut ,tuut");
     }
